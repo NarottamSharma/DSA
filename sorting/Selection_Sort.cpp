1731c45ce649067086bd03aaaa5cd69bb51645fc
Narottam Sharma <narottamaiengineer@gmail.com>
2025-04-01 03:52:23 +0000

    sorting

--- sorting/Selection_Sort.cpp
+++ sorting/Selection_Sort.cpp
@@ -3,16 +3,18 @@ using namespace std;
 
 void selectionSort(int arr[], int n) {
     for (int i = 0; i < n; i++) {
+        bool swapped = false;
         int min_idx = i;
-        for (int j = i ; j < n ; j++) { 
+        for (int j = i+1 ; j < n ; j++) { 
             if (arr[j] < arr[min_idx]) {
                 min_idx = j;
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+                swapped = true;
             }
         }
-        // Correct swap logic
-        int temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
+        if (!swapped) break;
     }
 }
 
