a2e2824a8e4fa307ce71d95bd8f0295d974b24db
Narottam Sharma <narottamdevengineer@gmail.com>
2025-07-27 13:19:18 +0530

    cmp

---
+++ 8.Strings/Introductionstr.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char str[] = {'a','b','c','\0'};
+    cout<<"string Length "<<strlen(str)<<endl;
+    char str1[199];
+    cin.getline(str1,100);
+    cout<<str<<endl;
+}
