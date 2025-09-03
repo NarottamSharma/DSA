a5e2d2e65a7d813e54a067f2bd5ef977e0d56bba
Narottam Sharma <narottamphodegaa@gmail.com>
2025-09-03 14:14:07 +0530

    Implement Fibonacci sequence generation and update output format

--- 0.TLEMentor/template.cpp
+++ 0.TLEMentor/template.cpp
@@ -11,8 +11,20 @@ const int INF = LLONG_MAX >>1;
 signed main(){
   fastio;
   int n; cin>>n;
-  for(int i=1;i<=n;i++){
-    cout<<i<<endl;
+  vector<int> v;
+  
+  if(n >= 1) v.push_back(0);
+  if(n >= 2) v.push_back(1);
+  
+  for(int i = 2; i < n; i++){
+    int third_value = v[i-1] + v[i-2];
+    v.push_back(third_value);
   }
-  return 0;
+  
+  // Output the Fibonacci sequence
+  for(int j = 0; j < n; j++){
+    cout << v[j] << " ";
+  }
+  cout << endl;
+
 }
